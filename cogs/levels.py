460959870008cfb4d057931a644476c528a9cb87
Mateusz <mateusz68latos@gmail.com>
2020-07-25 18:28:58 +0200

    established a connection to sqlite3

--- cogs/levels.py
+++ cogs/levels.py
@@ -1,15 +1,30 @@
 import discord
 from discord.ext import commands
 from discord.utils import get
-import json
-import random
+
+import sqlite3
 
 class LevelingSystem(commands.Cog):
-    """ Leveling system for discord
-    In progress
-    Sorry
-                - Termed
     """
+    Leveling System for Discord using MySQL
+    """
+
+    connection = sqlite3.connect('levels.db')
+    c = connection.cursor()
+
+    try:
+        c.execute("""CREATE TABLE levels (
+            memberID integer,
+            level integer,
+            experience integer
+            ) """)
+    except:
+        print(" Table is already made")
+
+    c.xe
+
+    connection.commit()
+    connection.close()
 
 
 def setup(bot):
