70fd517a628794df910386414c4bc5aadf17145c
Mateusz <mateusz68latos@gmail.com>
2020-07-25 17:59:40 +0200

    to fix : YoutubeSearch() returning a list with nothing

--- cogs/music.py
+++ cogs/music.py
@@ -1,13 +1,19 @@
-import pafy
-import vlc
+from discord.ext import commands
+from discord.utils import get
+import discord
+
+from youtube_search import YoutubeSearch
 
 class Music(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
 
-    def getUrlByMessage(ctx, message):
-        
+    @commands.command(pass_context=True, help='Drops some sick beat')
+    async def play(self, ctx, *,  phrase):
+        a = YoutubeSearch(phrase, max_results=10).to_dict()
+        print(a)
+
 
 def setup(bot):
     bot.add_cog(Music(bot))
