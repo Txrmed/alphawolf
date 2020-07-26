f6da2779d53bdddf92af0cc62d327eb67e67dca6
Mateusz <mateusz68latos@gmail.com>
2020-07-26 20:52:07 +0200

    fixed yotube search bug, changed to youtube-search-python

--- cogs/music.py
+++ cogs/music.py
@@ -2,7 +2,10 @@ from discord.ext import commands
 from discord.utils import get
 import discord
 
-from youtube_search import YoutubeSearch
+from youtubesearchpython import SearchVideos
+import os
+import Lavalink
+
 
 class Music(commands.Cog):
 
@@ -11,8 +14,23 @@ class Music(commands.Cog):
 
     @commands.command(pass_context=True, help='Drops some sick beat')
     async def play(self, ctx, *,  phrase):
-        a = YoutubeSearch(phrase, max_results=10).to_dict()
-        print(a)
+        search = SearchVideos(phrase, offset=1, mode="dict", max_results=1)
+        searchResults = search.result()
+
+        try:
+            url = searchResults["search_result"][0]["link"]
+            title = searchResults["search_result"][0]["title"]
+            duration = searchResults["search_result"][0]["duration"]
+            yt_channel = searchResults["search_result"][0]["channel"]
+        except IndexError:
+            await ctx.send("AlphaWolf nie znajdzie czegoś co ma za dużo słów więc postaraj się ujać tytuł w kilku słowach")
+
+        author_vc= ctx.author.voice
+        channel = ctx.author.voice.channel
+        if author_vc:
+            await author_vc.channel.connect()
+            print(" Connected to vc : {}".format(, author_vc.channel))
+
 
 
 def setup(bot):
