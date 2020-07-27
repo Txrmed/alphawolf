9c9cb951ef37d1fb9a25ec2bbe6a62583f2746ca
Mateusz <mateusz68latos@gmail.com>
2020-07-27 09:54:37 +0200

    added leave, pause, resume, stop functions

--- cogs/music.py
+++ cogs/music.py
@@ -6,6 +6,11 @@ from youtubesearchpython import SearchVideos
 import os
 import youtube_dl
 
+import _utils
+
+info = _utils.INFO_PREFIX
+error = _utils.ERROR_PREFIX
+
 ytdl_format_options = {
     'format': 'bestaudio/best',
     'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
@@ -53,11 +58,20 @@ class Music(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+
     @commands.command(pass_context=True, name='leave')
     async def leave(self, ctx):
-        if bot.voice_channe
+        server = ctx.message.guild.voice_client
+        channel = ctx.message.author.voice.channel
+        voice = get(self.bot.voice_clients, guild=ctx.guild)
+        if voice and voice.is_connected():
+            print(' [{}]Bot left channel : {}'.format(info, channel))
+            # await ctx.send(f'Left {channel}')
+            await server.disconnect()
+        else:
+            print(' [{}]Bot not in channel'.format(error))
 
-    @commands.command(pass_context=True, help='Drops some sick beat')
+    @commands.command(pass_context=True, help='Drops some sick beat', aliases=['p'])
     async def play(self, ctx, *,  phrase):
         search = SearchVideos(phrase, offset=1, mode="dict", max_results=1)
         searchResults = search.result()
@@ -70,15 +84,44 @@ class Music(commands.Cog):
         except IndexError:
             await ctx.send("AlphaWolf nie znajdzie czegoś co ma za dużo słów więc postaraj się ujać tytuł w kilku słowach")
 
+        voice = get(self.bot.voice_clients, guild=ctx.guild)
+
         author_vc= ctx.author.voice
         channel = ctx.author.voice.channel
         if author_vc:
             await author_vc.channel.connect()
-            print(" Connected to vc : {}".format(author_vc.channel))
+            print(" [{}]Connected to vc : {}".format(author_vc.channel))
 
         player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
         ctx.voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
 
+
+    @commands.command(pass_context=True, name='pause', aliases=['ps'])
+    async def pause(self, ctx):
+        voice = get(self.bot.voice_clients, guild = ctx.guild)
+        if voice and voice.is_playing():
+            print(' [{}]Paused Music'.format(info))
+            voice.pause()
+        else:
+            print(' [{}]Music not playing'.format(error))
+
+    @commands.command(pass_context=True, name='resume', aliases=['r'])
+    async def resume(self, ctx):
+        voice = get(self.bot.voice_clients, guild = ctx.guild)
+        if voice and voice.is_paused():
+            print(' [{}]Music resumed'.format(info))
+            voice.resume()
+        else:
+            print(' [{}]Music is not paused'.format(error))
+
+    @commands.command(pass_context=True, name='stop', aliases=['s'])
+    async def stop(ctx):
+        voice = get(self.bot.voice_clients, guild = ctx.guild)
+        if voice and voice.is_playing():
+            print(' [{}]Stopped music'.format(info))
+            voice.stop()
+        else:
+            print(' [{}]Music not playing'.format(error))
 
 
 def setup(bot):
