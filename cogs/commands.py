53d148c57be77e41369c4349f867e04af4739b4b
Mateusz <mateusz68latos@gmail.com>
2020-07-27 20:16:02 +0200

    Made a memes function to give juicy memes from 3 subreddits

--- cogs/commands.py
+++ cogs/commands.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from discord.utils import get
 
 import random
+import praw
 
 class Commands(commands.Cog):
 
@@ -12,6 +13,25 @@ class Commands(commands.Cog):
     @commands.command(pass_context=True, name='github', help='Gives the url to github')
     async def github(self, ctx):
         await ctx.send('Heres where the magic happens :' + ' https://github.com/Termed/alphawolf')
+
+    @commands.command(pass_context=True, name='meme', help='Throws memes at you')
+    async def meme(self, ctx):
+        r = praw.Reddit('bot1')
+
+        initialized = False
+        if initialized != True:
+            initialized = True
+            posts = []
+            for post in r.subreddit('memes').new():
+                posts.append(post.url)
+            for post in r.subreddit('dankmemes').new():
+                posts.append(post.url)
+            for post in r.subreddit('PewdiepieSubmissions').new():
+                posts.append(post.url)
+
+        meme = posts[random.randint(0, len(posts) - 1)]
+        print(" [INFO] Sent meme : {}".format(meme))
+        await ctx.send(meme)
 
 def setup(bot):
     bot.add_cog(Commands(bot))
