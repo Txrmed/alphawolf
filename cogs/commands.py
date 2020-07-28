b1abd2a4aa8ec4930b9ea4b9d8a2508956b8dc37
Mateusz <mateusz68latos@gmail.com>
2020-07-28 13:37:20 +0200

    Made .memes work, Statistically refreshes every 100 calls

--- cogs/commands.py
+++ cogs/commands.py
@@ -4,34 +4,40 @@ from discord.utils import get
 
 import random
 import praw
+import time
+
+memes = []
+
+index = range(1, 100)
+if index[random.randint(0, len(index))] == 69:
+    memes = []
+    print(' [INFO] Refreshed memes array')
 
 class Commands(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-
     @commands.command(pass_context=True, name='github', help='Gives the url to github')
     async def github(self, ctx):
         await ctx.send('Heres where the magic happens :' + ' https://github.com/Termed/alphawolf')
 
     @commands.command(pass_context=True, name='meme', help='Throws memes at you')
-    async def meme(self, ctx):
+    async def meme(self, ctx, init=False):
         r = praw.Reddit('bot1')
+        meme = None
 
-        initialized = False
-        if initialized != True:
-            initialized = True
-            posts = []
-            for post in r.subreddit('memes').new():
-                posts.append(post.url)
-            for post in r.subreddit('dankmemes').new():
-                posts.append(post.url)
-            for post in r.subreddit('PewdiepieSubmissions').new():
-                posts.append(post.url)
+        if len(memes) == 0:
+            for post in r.subreddit('memes').hot():
+                url = post.url
+                memes.append(url)
+        else:
+            meme = memes[random.randint(0, len(memes))]
 
-        meme = posts[random.randint(0, len(posts) - 1)]
+        if meme == None:
+            meme = memes[random.randint(0, len(memes))]
         print(" [INFO] Sent meme : {}".format(meme))
         await ctx.send(meme)
+
 
 def setup(bot):
     bot.add_cog(Commands(bot))
