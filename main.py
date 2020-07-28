838098cb521cc812bd00d502506dfc2138d87041
Mateusz <mateusz68latos@gmail.com>
2020-07-28 13:57:13 +0200

    trying to fix git

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import discord
 from discord.ext import commands
 from discord.utils import get
+
 import os
 from dotenv import load_dotenv
 import random
 
+import cogs._utils as utils
+
 load_dotenv('X:\\Code\\.env')
 token = os.getenv('TOKEN')
 
-bot = commands.Bot(command_prefix="sudo")
+bot = commands.Bot(command_prefix=utils.COMMAND_PREFIX)
+
+
 
 @bot.event
 async def on_ready():
+
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.streaming, name="over my servers"))
 
     with open("ascii.txt", "r") as f:
         print(f.read())
@@ -20,7 +27,6 @@ async def on_ready():
     for file in os.listdir('./cogs'):
         if file.endswith('.py') and not file.startswith('_'):
             bot.load_extension(f'cogs.{file[:-3]}')
-            print(' Loaded {}'.format(file))
-
+            print(' [INFO] Loaded {}'.format(file))
 
 bot.run(token)
