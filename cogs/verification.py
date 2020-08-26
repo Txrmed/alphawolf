8bbd3347e8285ae22d2c156cdc5059597809ea07
Mateusz <mateusz68latos@gmail.com>
2020-08-26 14:33:51 +0200

    made a working verification system

--- cogs/verification.py
+++ cogs/verification.py
@@ -10,14 +10,28 @@ class Verification(commands.Cog):
     @commands.command(name='generate_verification', help='Generates verification message')
     async def generate_verification(self, ctx):
         message = await ctx.send(" Click the ✅ reaction to verify yourself")
-        await message.add_reaction("✅")
         verification_message_id = message.id
         print(' [INFO] Added Verification message. ID : {}'.format(verification_message_id))
+
+        # Check if Verification role already exists
+
+        if get(ctx.guild.roles, name="Verified"): 
+            pass
+        else:
+            perms = discord.Permissions()
+            perms.general()
+            perms.text()
+            perms.voice()                            
+
+            await ctx.guild.create_role(name="Verified", permissions=perms, colour=discord.Colour(0x00bd09))
+
+        await message.add_reaction("✅")
+        
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
         guild = self.bot.get_guild(payload.guild_id)
-        role = discord.utils.get(guild.roles, name="Zweryfikowany")
+        role = discord.utils.get(guild.roles, name="Verified")
         await payload.member.add_roles(role, reason="Verified Self", atomic=True)
 
 
