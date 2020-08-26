dcbf71a1499f3532956579286ccd7fd6e35596b3
Mateusz <mateusz68latos@gmail.com>
2020-08-26 14:33:14 +0200

    trying to make a .dat file

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 import discord
 from discord.ext import commands
 from discord.utils import get
+from dotenv import load_dotenv
 
 import os
-from dotenv import load_dotenv
-import random
+import os.path
 
-import cogs._utils as utils
+load_dotenv()
+token = os.getenv("TOKEN")  
+prefix = os.getenv("PREFIX")
 
-load_dotenv('X:\\Code\\.env')
-token = os.getenv('TOKEN')
-
-bot = commands.Bot(command_prefix=utils.COMMAND_PREFIX)
-
+bot = commands.Bot(command_prefix=prefix)
 
 
 @bot.event
@@ -24,9 +22,21 @@ async def on_ready():
         print(f.read())
 
     print(' ----------')
+    num = 0
+    for file in os.listdir("./cogs"):
+        if file.endswith('.py'):
+            num += 1
+
+    loaded = 0
     for file in os.listdir('./cogs'):
         if file.endswith('.py') and not file.startswith('_'):
+            loaded += 1
             bot.load_extension(f'cogs.{file[:-3]}')
-            print(' [INFO] Loaded {}'.format(file))
+            print(' [INFO] Loaded {} : {}/{}'.format(file, loaded, num))
+
+    if os.isfile("cogs/data.dat"):
+        pass
+    else:
+        os.mkdir("cogs/data.dat")
 
 bot.run(token)
