fc35fb1b2cc236b2da8e68e5c0bb43211e3434b0
Mateusz <mateusz68latos@gmail.com>
2020-08-27 09:41:15 +0200

    fixed on_message

--- cogs/filters.py
+++ cogs/filters.py
@@ -2,17 +2,15 @@ import discord
 import os
 from discord.ext import commands
 from discord.utils import get
-import asyncio
 
 class onMessage(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.Cog.listener()
-    async def check_for_capitals(self, ctx, message):
+    async def on_message(self, message):
         message_content = message.content
         if message_content.isupper():
-            message_author = message.author
             await message.channel.send('{} , Please refrain from using too many capital letters.'.format(message.author.mention))
 
 
